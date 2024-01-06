089c641745423ddb0aa76d589274580f8cb09372
sidxh <totallysiddhant@gmail.com>
2024-01-06 21:08:18 +0530

    feat: setup prisma sync with oauth and db

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,11 +1,15 @@
-import type { NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import type { Adapter } from 'next-auth/adapters';
+import type { NextAuthOptions } from 'next-auth';
+import DiscordProvider from 'next-auth/providers/discord';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from './db';
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID as string,
       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
     }),
   ],
-} satisfies NextAuthOptions;
+};
