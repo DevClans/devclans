6ebcfaea963a2a7fd487fa586dd40550882a130c
sidxh <totallysiddhant@gmail.com>
2024-01-06 20:08:42 +0530

    styles: checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="text-center my-10">
-      <h1 className='font-bold text-4xl'>DevClans</h1>
+      <h1 className='font-bold text-4xl'>DevClaned</h1>
     </main>
   )
 }
