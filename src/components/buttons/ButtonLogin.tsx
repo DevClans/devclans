69a9bb8c9dec259c65716549f72d1130a32f4c6a
sidxh <totallysiddhant@gmail.com>
2024-01-07 14:25:14 +0530

    feat: added toasts for succesful login and logouts

--- src/components/buttons/ButtonLogin.tsx
+++ src/components/buttons/ButtonLogin.tsx
@@ -1,20 +1,46 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ButtonLogin = () => {
   const { data: session } = useSession();
 
-  const handleClick = () => {
+  useEffect(() => {
+    // Clear previous toasts when the component mounts
+    toast.dismiss();
+
+    // Check if the session is present (user is logged in) and show a toast
     if (session) {
-      signOut();
-    } else {
-      signIn();
+      toast.success("Successfully Logged In");
     }
+  }, []); // Re-run the effect when the session changes
+
+  useEffect(() => {
+    // Check if the session is not present (user is logged out) and show a toast
+    if (!session) {
+      toast.info("Successfully Logged Out");
+    }
+  }, [session]); // Re-run the effect when the session changes
+
+  const handleLogin = () => {
+    signIn();
   };
+
+  const handleLogout = () => {
+    signOut();
+  };
+
   return (
-    <button onClick={handleClick} id="btnLogin">
-      {session ? "Logout" : "Login"}
-    </button>
+    <>
+      <button onClick={session ? handleLogout : handleLogin} id="btnLogin">
+        {session ? "Logout" : "Login"}
+      </button>
+      <ToastContainer
+        theme="dark"
+      />
+    </>
   );
 };
 
