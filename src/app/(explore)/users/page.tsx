6649ac4cb1a6322e2cddc205f8cf451e8f68c509
sidxh <totallysiddhant@gmail.com>
2024-01-09 00:56:33 +0530

    fix: api/project and api/user routes

--- src/app/(explore)/users/page.tsx
+++ src/app/(explore)/users/page.tsx
@@ -1,7 +1,32 @@
+"use client"
+import { useSession } from 'next-auth/react';
+
 const page = () => {
-  return <>
-    <h1>Users</h1>
-  </>;
+  const { data: session } = useSession();
+
+  // Check if the user is authenticated
+  if (session) {
+    // Access user data from the session object
+    const { user } = session;
+
+    // Now you can use the user object to display user information
+    return (
+      <div className='border-2 border-red-300 mx-auto text-center'>
+        <h1 className='my-10'>Welcome, {user?.name}!</h1>
+        <h2 >Email: {user?.email}</h2>
+        <h2 >Email: {user?.image}</h2>
+        {/* Add more user data as needed */}
+      </div>
+    );
+  }
+
+  // If the user is not authenticated, you can display a login link or other content
+  return (
+    <div>
+      <p>Please sign in to view your profile.</p>
+      {/* You can add a login button or link here */}
+    </div>
+  );
 };
 
 export default page;
--- src/app/api/project/new/route.ts
+++ src/app/api/project/new/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-
 async function handler(req:Request) {
     const { userId, projectName, projectDescription } = await req.json();
 
