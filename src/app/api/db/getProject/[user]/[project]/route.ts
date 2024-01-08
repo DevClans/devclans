d05f87c92bd8c1b28b1e26e93286c8e4d54bd23d
sidxh <totallysiddhant@gmail.com>
2024-01-08 18:31:17 +0530

    fix: type error request

--- src/app/api/db/getProject/[user]/[project]/route.ts
+++ src/app/api/db/getProject/[user]/[project]/route.ts
@@ -1,25 +1,34 @@
-import { NextRequest, NextResponse } from 'next/server';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
 
-async function handler(req: NextRequest,{ params }:{ params : { user: string, project: string }}) {
-    
-    const  { user, project }  = params;
-  
-    if (!user || typeof user !== 'string') {
-        return NextResponse.json({ message: 'Invalid user parameter' });
-      }
+async function handler(
+    req: NextApiRequest, 
+    res: NextApiResponse
+) {
+    const { user, project } = req.query;
 
-   
-    const projects = await prisma.project.findUnique({
-        where: {
-          ownerId : user,
-          id: project
-        },
-      });
-      
-     
-      return  NextResponse.json(projects);
+    if (!user || typeof user !== 'string') {
+        return res.status(400).json({ message: 'Invalid user parameter' });
+    }
+
+    try {
+        const projectData = await prisma.project.findUnique({
+            where: {
+                ownerId: user,
+                id: project
+            },
+        });
+
+        if (projectData) {
+            return res.status(200).json(projectData);
+        } else {
+            return res.status(404).json({ message: 'Project not found' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error' });
+    }
 }
 
-export { handler as GET }
+export { handler as GET };
