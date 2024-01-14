f3b0e488686561beee84a274f4eabd82bd927028
Satvik1769 <manchandasatvik@gmail.com>
2024-01-14 23:59:02 +0530

    up

--- src/app/api/db/getUser/route.ts
+++ src/app/api/db/getUser/route.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function handler(req: Request) {
+    const x=0;
+
     const user = await prisma.user.findMany();
     return  NextResponse.json(user);
 }
