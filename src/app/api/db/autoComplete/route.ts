3e9d6a78d320332993f2cda7e0fe07f840852f0c
Satvik1769 <manchandasatvik@gmail.com>
2024-01-16 21:33:07 +0530

    search and autocomplete api

---
+++ src/app/api/db/autoComplete/route.ts
@@ -0,0 +1,50 @@
+import dbConnect from '@/lib/dbConnect';
+import { NextResponse } from 'next/server';
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.DATABASE_URL!);
+
+async function handler(req:Request) {
+  await dbConnect();
+
+  try {
+    await client.connect();
+    console.log("connected");
+    const db = client.db("user");
+    const collection = db.collection("projects");
+    
+    const { searchTerm } = await req.json();
+    
+    const pipeline = [
+      {
+        $search: {
+          index: "default",
+          "autocomplete":{
+            "query":searchTerm,
+            "path":"title",
+            "tokenOrder":"sequential"
+          }
+        },
+      },
+      {
+        $limit:10
+      },
+      {
+        $project:{
+            "title":1
+        }
+      }
+    ];
+
+    const searchResults = await collection.aggregate(pipeline).toArray();
+
+    return NextResponse.json({ results: searchResults });
+  } catch (error) {
+    console.error('Error searching:', error);
+   return  NextResponse.json({ message: 'Internal Server Error' });
+  } finally {
+    await client.close();
+  }
+}
+
+export { handler as POST };
