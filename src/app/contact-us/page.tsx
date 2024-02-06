4d03f30f38e00defc89e3a3fc1d864eab8085cf4
auspy <vinayak.kshetez0102@gmail.com>
2024-02-06 14:46:38 +0530

    chore: change contact-us hero

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -4,6 +4,7 @@ import { FC } from "react";
 import { useForm } from "react-hook-form";
 import { sendEmail } from "@/utils/send-email";
 import { ButtonBlue } from "@/components";
+import CommonHero from "@/components/CommonHero";
 
 export type FormData = {
   name: string;
@@ -19,55 +20,57 @@ const Contact: FC = () => {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="container w-full gap-3 p-6 fcc max-w-md mx-auto"
-    >
-      <h1 className="text-[36px]">Contact Form</h1>
-      <div className=" w100">
-        <label
-          htmlFor="name"
-          className="mb-1 block text-xs font-medium text-text"
-        >
-          Full Name
-        </label>
-        <input
-          type="text"
-          placeholder="Full Name"
-          className="w-full border    p-3 text-sm font-medium  outline-none focus:border-priDark focus:shadow-md"
-          {...register("name", { required: true })}
-        />
-      </div>
-      <div className=" w100">
-        <label
-          htmlFor="email"
-          className="mb-1 block text-xs font-medium text-text"
-        >
-          Email Address
-        </label>
-        <input
-          type="email"
-          placeholder="example@domain.com"
-          className="w-full border    p-3 text-sm font-medium  outline-none focus:border-priDark focus:shadow-md"
-          {...register("email", { required: true })}
-        />
-      </div>
-      <div className="w100">
-        <label
-          htmlFor="message"
-          className="mb-1 block text-xs font-medium text-text"
-        >
-          Message
-        </label>
-        <textarea
-          rows={4}
-          placeholder="Type your message"
-          className="w-full resize-none border  p-3 text-sm font-medium  outline-none focus:border-priDark focus:shadow-md"
-          {...register("message", { required: true })}
-        ></textarea>
-      </div>
-      <ButtonBlue label="Submit" className="w100" />
-    </form>
+    <>
+      <CommonHero heading="Contact Us" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="container w-full gap-3 p-6 fcc max-w-md mx-auto"
+      >
+        <div className=" w100">
+          <label
+            htmlFor="name"
+            className="mb-1 block text-xs font-medium text-text"
+          >
+            Full Name
+          </label>
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="w-full border    p-3 text-sm font-medium  outline-none focus:border-priDark focus:shadow-md"
+            {...register("name", { required: true })}
+          />
+        </div>
+        <div className=" w100">
+          <label
+            htmlFor="email"
+            className="mb-1 block text-xs font-medium text-text"
+          >
+            Email Address
+          </label>
+          <input
+            type="email"
+            placeholder="example@domain.com"
+            className="w-full border    p-3 text-sm font-medium  outline-none focus:border-priDark focus:shadow-md"
+            {...register("email", { required: true })}
+          />
+        </div>
+        <div className="w100">
+          <label
+            htmlFor="message"
+            className="mb-1 block text-xs font-medium text-text"
+          >
+            Message
+          </label>
+          <textarea
+            rows={4}
+            placeholder="Type your message"
+            className="w-full resize-none border  p-3 text-sm font-medium  outline-none focus:border-priDark focus:shadow-md"
+            {...register("message", { required: true })}
+          ></textarea>
+        </div>
+        <ButtonBlue label="Submit" className="w100" />
+      </form>
+    </>
   );
 };
 
