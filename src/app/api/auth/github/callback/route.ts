10b7fa4c545652c146ebec818076b41c6b38ac04
auspy <vinayak.kshetez0102@gmail.com>
2024-02-08 02:09:01 +0530

    fix: github callback for new user

--- src/app/api/auth/github/callback/route.ts
+++ src/app/api/auth/github/callback/route.ts
@@ -1,23 +1,31 @@
 import dbConnect from "@/lib/dbConnect";
 import { UserModel } from "@/mongodb/models";
-import { Types } from "mongoose";
+import redisClient from "@/redis/config";
+import { UserRedisKeys } from "@/types/mongo/user.types";
+import { zodUserGithubDetailsSchema } from "@/zod/zod.common";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const baseUrl = req.nextUrl.origin + "/user/new";
+  const code = req.nextUrl.searchParams.get("code");
+  const userid = req.nextUrl.searchParams.get("userid");
+  const newParams = new URLSearchParams();
+  const baseUrl = req.nextUrl.origin + "/user/" + userid;
   try {
-    const code = req.nextUrl.searchParams.get("code");
-    const userid = req.nextUrl.searchParams.get("userid");
-    console.log(code, "code", userid, "userid");
+    newParams.set("mode", "edit");
+    // console.log(code, "code", userid, "userid");
     if (!code) {
-      return NextResponse.redirect(baseUrl + "?error=code_not_found");
+      console.error("code not found");
+      newParams.set("error", "code_not_found");
+      return NextResponse.redirect(baseUrl + "?" + newParams.toString());
     }
 
     const GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
     const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
     console.log(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, "tokens");
     if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
-      return NextResponse.redirect(baseUrl + "?error=vars_not_set");
+      console.error("vars not set");
+      newParams.set("error", "vars_not_set");
+      return NextResponse.redirect(baseUrl + "?" + newParams.toString());
     }
 
     // Exchange the code for a token
@@ -41,7 +49,9 @@ export async function GET(req: NextRequest) {
     const accessToken = tokenData.access_token;
 
     if (!accessToken) {
-      return NextResponse.redirect(baseUrl + "?error=access_token_not_found");
+      console.error("access token not found");
+      newParams.set("error", "access_token_not_found");
+      return NextResponse.redirect(baseUrl + "?" + newParams.toString());
     }
 
     // Fetch user profile
@@ -54,11 +64,14 @@ export async function GET(req: NextRequest) {
     const userProfile = await userProfileResponse.json();
 
     // Redirect back to the new user page with the GitHub username as a query parameter
-    console.log(userProfile, "userProfile");
+    // console.log(userProfile, "userProfile");
     // send user github data to mongodb
     if (!userid) {
-      return NextResponse.redirect(baseUrl + "?error=userid_not_found");
+      console.error("userid not found");
+      newParams.set("error", "userid_not_found");
+      return NextResponse.redirect(baseUrl + "?" + newParams.toString());
     }
+    console.log("updating user github details");
     await dbConnect();
     const updatedUser = await UserModel.findOneAndUpdate(
       { _id: userid },
@@ -71,11 +84,30 @@ export async function GET(req: NextRequest) {
 
       { new: true } // Return the updated document
     );
+    console.info("user updated successfully");
+    // add user data in cache
+    try {
+      console.log("trying to add user data in cache");
+      const githubData = zodUserGithubDetailsSchema.safeParse(
+        updatedUser.githubDetails
+      );
+      if (githubData.success) {
+        console.log("adding user github data in cache");
+        await redisClient.hset(
+          UserRedisKeys.github,
+          userid,
+          JSON.stringify(githubData.data)
+        );
+      } else {
+        console.error("github data not valid", githubData.error);
+      }
+    } catch (error) {
+      console.error("error adding user github data in cache", error);
+    }
 
     console.log(updatedUser, "user");
-    const redirectUrl = `${baseUrl}?githubUsername=${encodeURIComponent(
-      userProfile.login
-    )}`;
+    newParams.set("githubUsername", userProfile.login);
+    const redirectUrl = `${baseUrl}?${newParams.toString()}`;
     return NextResponse.redirect(redirectUrl);
   } catch (error) {
     console.log(error, "error");
