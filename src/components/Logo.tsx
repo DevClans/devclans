eca04108f3307925e43d031c744c45f496fc013b
auspy <vinayak.kshetez0102@gmail.com>
2024-02-10 00:15:24 +0530

    feat: added logo and favicon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
   return (
     <>
       <Link title="Devclans Logo" href={"/"} className="relative" id="logo">
-        Devclans
+        <Image src={"/logo.png"} alt="devclans logo" width={76} height={30} />
         <p
           style={{
             fontFamily: "var(--poppins)",
