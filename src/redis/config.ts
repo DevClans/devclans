a662c584fadef4d4ebac9c39c2ad39d713e12ae9
auspy <vinayak.kshetez0102@gmail.com>
2024-02-09 20:02:51 +0530

    fix: redis url for production added

--- src/redis/config.ts
+++ src/redis/config.ts
@@ -1,7 +1,7 @@
 import { env } from "node:process";
 import { Redis } from "ioredis";
 
-const redisClient = new Redis("redis://localhost:6379");
+const redisClient = new Redis(env.REDIS_URL as string);
 
 redisClient.on("connect", () => {
   console.log("Redis connected", process.env.PORT);
