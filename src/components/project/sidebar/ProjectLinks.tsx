ff34467a94525221ed4222fd2c9b5e7e8dd63f7b
auspy <vinayak.kshetez0102@gmail.com>
2024-02-12 08:26:56 +0530

    feat: about page

--- src/components/project/sidebar/ProjectLinks.tsx
+++ src/components/project/sidebar/ProjectLinks.tsx
@@ -1,13 +1,12 @@
 "use client";
-import { IconLink, SidebarList } from "@/components";
+import { SidebarList } from "@/components";
 import { ListItemProps } from "@/types/list.types";
 import copyToClipboard from "@/lib/copyToClipboard";
-import { selectIconForLinks, socialIcons } from "@/lib/socialIcons";
+import { selectIconForLinks } from "@/lib/socialIcons";
 import { ProjectProps } from "@/types/mongo/project.types";
 import ButtonLinkIcon from "@/components/buttons/ButtonLinkIcon";
 
 const ProjectLinks = ({ links }: { links: ProjectProps["projectLinks"] }) => {
-  const dummyLink = "https://github.com/username/repo";
   const linkss: ListItemProps[] = links.map((link) => {
     return {
       text: link?.split(".com/")?.[1],
