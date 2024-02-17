2ca7b513930d1a56361c2eac5d62bbb8643ec5e5
sidxh <totallysiddhant@gmail.com>
2024-02-18 03:27:47 +0530

    fix: improved filters

--- src/lib/memberLevel.tsx
+++ src/lib/memberLevel.tsx
@@ -2,11 +2,6 @@ export const memberLevelsMap = {
   beginner: true,
   intermediate: true,
   advanced: true,
-  expert: true,
-  master: true,
-  grandmaster: true,
-  legend: true,
-  god: true,
 };
 
 export const memberLevels = [...Object.keys(memberLevelsMap), ""] as const;
