ba17a79ade9942e63020c4c088dc442ad3ec623e
auspy <vinayak.kshetez0102@gmail.com>
2024-02-20 23:46:55 +0530

    feat: github routes

---
+++ src/app/github_connect/error/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <div>
+      <h2>GitHub Data Update Failed!</h2>
+      <p>Sorry for the inconvenience, please try again later</p>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/github_connect/success/page.tsx
@@ -0,0 +1,12 @@
+const page = () => {
+  return (
+    <div>
+      <h2>GitHub Data Updated!</h2>
+      <p>
+        Reload <b>the form page</b> to view changes
+      </p>
+    </div>
+  );
+};
+
+export default page;
