c40d84dab13a24f30e89f53464863b46f1f57c78
auspy <vinayak.kshetez0102@gmail.com>
2024-02-25 12:34:37 +0530

    fix: prevent /explore/explore

--- src/components/listItems/UserItem.tsx
+++ src/components/listItems/UserItem.tsx
@@ -78,7 +78,7 @@ const UserItem = async ({
         detailHeading={"Skill Level"}
         detailDesc={skillLevel as string}
         chipArr={skills}
-        baseUrl={"/user/"}
+        baseUrl={"/user"}
         _id={_id?.toString()}
         desc={bio || ""}
       />
