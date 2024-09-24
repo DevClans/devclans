016e1292459140f5ee38b04d74c365f481ee8bab
Kshetez Vinayak <100942053+auspy@users.noreply.github.com>
2024-09-24 13:02:28 +0530

    Update mongodb.config.ts

--- src/utils/mongodb.config.ts
+++ src/utils/mongodb.config.ts
@@ -1,10 +1,10 @@
 import { MongoClient } from "mongodb";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+if (!process.env.MONGODB_URL) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URL"');
 }
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URL;
 const options = {};
 
 let client;
